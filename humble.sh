6c96adce0bf89e9220b7ea67b977295ecf843dd1
nohaismaileng <noha.ismail.eng@gmail.com>
2026-09-08 16:06:24 +0300

    Add script to start Docker and launch ROS2 tools

---
+++ humble.sh
@@ -0,0 +1,12 @@
+sudo docker start boring_williamson
+
+sudo docker exec -it -e DISPLAY=$DISPLAY boring_williamson bash
+
+source /opt/ros/humble/setup.bash
+
+cd /ros2_ws
+source install/setup.bash
+
+gazebo
+or
+rviz2
