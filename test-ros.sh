af05ac29f4db9b5d85ed692253985a4b4e49f01f
nohaismaileng <noha.ismail.eng@gmail.com>
2026-09-08 16:15:06 +0300

    Add comment on ROS versions and Docker command
    Added a comment about ROS versions and a command to list Docker containers.

--- test-ros.sh
+++ test-ros.sh
@@ -11,3 +11,8 @@ sudo docker exec -it boring_williamson bash
 source /opt/ros/humble/setup.bash
 
 ros2 run demo_nodes_cpp listener
+
+
+//what do u have humble or jazzy
+
+sudo docker ps -a --format "table {{.Names}}\t{{.Image}}\t{{.Status}}"
