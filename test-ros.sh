39fb900fc8ec68067d2cc0d4659d6833bac473ae
nohaismaileng <noha.ismail.eng@gmail.com>
2026-09-08 16:12:55 +0300

    Create test-ros.sh for ROS 2 demo nodes
    Add script to run ROS 2 talker and listener in Docker

---
+++ test-ros.sh
@@ -0,0 +1,13 @@
+sudo docker exec -it boring_williamson bash
+
+source /opt/ros/humble/setup.bash
+
+ros2 run demo_nodes_cpp talker
+
+//////////////////other terminal
+
+sudo docker exec -it boring_williamson bash
+
+source /opt/ros/humble/setup.bash
+
+ros2 run demo_nodes_cpp listener
